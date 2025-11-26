66585d6afba994dcad688ff9c8472490fa378930
facundo <juliabarrios782@gmail.com>
2025-11-26 14:43:00 -0300

    added tiled and test for level

---
+++ POCHAOSLYPSE/tiled/patron1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="patron" tilewidth="16" tileheight="16" tilecount="143" columns="13">
+ <image source="../Content/Another Metroidvania Asset Pack Vol. 1 ver. 1.5/Tilesets/library/tileset_library.png" width="208" height="176"/>
+</tileset>
